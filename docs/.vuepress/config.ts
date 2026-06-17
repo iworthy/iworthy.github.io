8c1383d02b22dd418777ee597f0bbae227401de3
worthy <admin@worthy.cc>
2026-06-17 22:35:41 +0800

    Align blog hostname with apex domain

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,7 +29,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    hostname: 'https://www.worthy.cc',
+    hostname: 'https://worthy.cc',
 
     /* 文档仓库配置，用于 editLink */
     // docsRepo: '',
